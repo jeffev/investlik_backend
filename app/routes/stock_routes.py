b44a469918355649ed3e154be0a1e8d3a4162633
JEFFE-NOTE\jeffe <jeffe>
2024-04-29 23:08:53 -0300

    fixado bug na edição da ação

--- app/routes/stock_routes.py
+++ app/routes/stock_routes.py
@@ -66,12 +66,41 @@ def edit_stock_json(id):
         return jsonify({'message': 'Stock not found'}), 404
     try:
         stock_data = request.get_json()
-        stock.companyid = stock_data['companyid']
-        stock.companyname = stock_data['companyname']
-        stock.ticker = stock_data['ticker']
-        stock.price = float(stock_data['price'])
-        stock.p_l = float(stock_data['p_l'])
-        # Continue for other attributes...
+        stock.companyid=stock_data['companyid'],
+        stock.companyname=stock_data['companyname'],
+        stock.ticker=stock_data['ticker'],
+        stock.price=float(stock_data['price']),
+        stock.p_l=float(stock_data['p_l']),
+        stock.p_vp=float(stock_data['p_vp']),
+        stock.p_ebit=float(stock_data['p_ebit']),
+        stock.p_ativo=float(stock_data['p_ativo']),
+        stock.ev_ebit=float(stock_data['ev_ebit']),
+        stock.margembruta=float(stock_data['margembruta']),
+        stock.margemebit=float(stock_data['margemebit']),
+        stock.margemliquida=float(stock_data['margemliquida']),
+        stock.p_sr=float(stock_data['p_sr']),
+        stock.p_capitalgiro=float(stock_data['p_capitalgiro']),
+        stock.p_ativocirculante=float(stock_data['p_ativocirculante']),
+        stock.giroativos=float(stock_data['giroativos']),
+        stock.roe=float(stock_data['roe']),
+        stock.roa=float(stock_data['roa']),
+        stock.roic=float(stock_data['roic']),
+        stock.dividaliquidapatrimonioliquido=float(stock_data['dividaliquidapatrimonioliquido']),
+        stock.dividaliquidaebit=float(stock_data['dividaliquidaebit']),
+        stock.pl_ativo=float(stock_data['pl_ativo']),
+        stock.passivo_ativo=float(stock_data['passivo_ativo']),
+        stock.liquidezcorrente=float(stock_data['liquidezcorrente']),
+        stock.peg_ratio=float(stock_data['peg_ratio']),
+        stock.receitas_cagr5=float(stock_data['receitas_cagr5']),
+        stock.vpa=float(stock_data['vpa']),
+        stock.lpa=float(stock_data['lpa']),
+        stock.valormercado=float(stock_data['valormercado']),
+        stock.segmentid=stock_data['segmentid'],
+        stock.sectorid=stock_data['sectorid'],
+        stock.subsectorid=stock_data['subsectorid'],
+        stock.subsectorname=stock_data['subsectorname'],
+        stock.segmentname=stock_data['segmentname'],
+        stock.sectorname=stock_data['sectorname']
         db.session.commit()
         return jsonify({'message': 'Stock edited successfully'})
     except Exception as e:
